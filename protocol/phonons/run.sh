b896782a2fc7f7b0a0368bcbb3be54a781e8f8d8
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-09-22 15:43:42 +0200

    Update run.sh

--- protocol/phonons/run.sh
+++ protocol/phonons/run.sh
@@ -9,9 +9,9 @@
 # output.json
 # plot.nbconvert.ipynb
 
-conda env create --name $1 --file=${script_dir}/environments.yml
-conda activate $1 
+# conda env create --name $1 --file=${script_dir}/environments.yml
+# conda activate $1 
 papermill ${script_dir}/script.ipynb -k 'python3' -p input_file $2 -p output_file $3
 papermill ${script_dir}/plot.ipynb $4 -k 'python3' -p input_file $3
-conda deactivate
-conda remove --name $1 --all
+# conda deactivate
+# conda remove --name $1 --all
